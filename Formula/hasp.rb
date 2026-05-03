9ba1fbe958f6843ca75d1773cefc7f4eb7a5f320
ehmo <r@turek.co>
2026-05-02 20:15:04 -0700

    Update HASP formula for v0.1.56

--- Formula/hasp.rb
+++ Formula/hasp.rb
@@ -1,26 +1,26 @@
 class Hasp < Formula
   desc "Local-first broker for managed secrets in agent workflows"
   homepage "https://gethasp.com"
-  version "0.1.39"
+  version "0.1.56"
   license :cannot_represent
   on_macos do
     on_arm do
-      url "https://downloads.gethasp.com/hasp/releases/v0.1.39/hasp_0.1.39_darwin_arm64.tar.gz"
-      sha256 "49259c588a47241de3862cb30e0b014d7de952da0622be37d11bc5b9cb1e5fcc"
+      url "https://downloads.gethasp.com/hasp/releases/v0.1.56/hasp_0.1.56_darwin_arm64.tar.gz"
+      sha256 "75cbfff3db89ce4be2cb31b986605d8c2a6bc3dbb4c2d1d10b43002973cdc4aa"
     end
     on_intel do
-      url "https://downloads.gethasp.com/hasp/releases/v0.1.39/hasp_0.1.39_darwin_amd64.tar.gz"
-      sha256 "033fe2ef052cbe8f7c4d4bba293d5eb761bc85c095d1c3e9c71001321192b9eb"
+      url "https://downloads.gethasp.com/hasp/releases/v0.1.56/hasp_0.1.56_darwin_amd64.tar.gz"
+      sha256 "5764a3de15dbff3c70f54b17bfcee9f1a60bf3b77726cba2eaf9a827178e770c"
     end
   end
   on_linux do
     on_arm do
-      url "https://downloads.gethasp.com/hasp/releases/v0.1.39/hasp_0.1.39_linux_arm64.tar.gz"
-      sha256 "b82b9bf878204978b2bacccbf1fd054000bef80a6067a46f87d7d015e524fd51"
+      url "https://downloads.gethasp.com/hasp/releases/v0.1.56/hasp_0.1.56_linux_arm64.tar.gz"
+      sha256 "9f315f6663b5fb21fb10ec6c70299c1df3cc58889147024f8c84a337df6d2ca7"
     end
     on_intel do
-      url "https://downloads.gethasp.com/hasp/releases/v0.1.39/hasp_0.1.39_linux_amd64.tar.gz"
-      sha256 "c495ca3638b988997be46f59d875143f05b8e94fe11543f30bd00ed58cb51602"
+      url "https://downloads.gethasp.com/hasp/releases/v0.1.56/hasp_0.1.56_linux_amd64.tar.gz"
+      sha256 "f6f9cc4cf69184896098ce6f6d8e2f4b85acc8ca97845406402994e8d622457d"
     end
   end
   def install
